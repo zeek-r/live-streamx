d25b0f399f53b3df9db0a4af0e5879f5fac4d5a0
zeek-r <bistaroshan03@gmail.com>
2020-05-09 22:40:34 +0545

    Added try catch

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ const config = require("./commons/config");
 
 const main = async () => {
   // const app = services.init(config);
-  const { server } = await socket.init();
-  server.listen(config.apiPort, () => {
-    console.log(`Server started at ${config.apiPort}`);
-  });
+  try {
+    const { server } = await socket.init();
+    server.listen(config.apiPort, () => {
+      console.log(`Server started at ${config.apiPort}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 main();
